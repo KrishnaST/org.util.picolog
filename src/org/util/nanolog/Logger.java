da646a8b97a71a618b9f72812dad992f6af23244
krishna.telgave <krishna.telgave@SIL_DEV076.saas.SILASPDC.com>
2019-07-14 18:32:41 +0530

    added two new methods.

--- src/org/util/nanolog/Logger.java
+++ src/org/util/nanolog/Logger.java
@@ -15,6 +15,7 @@ import java.util.Date;
 import java.util.Set;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 import org.util.nanolog.internals.BufferedDailyLogger;
 import org.util.nanolog.internals.BufferedLogger;
@@ -25,21 +26,21 @@ import org.util.nanolog.internals.InstantLogger;
 
 public abstract class Logger implements AutoCloseable {
 
-	private static final StackWalker		sw			= StackWalker.getInstance(Set.of(Option.RETAIN_CLASS_REFERENCE), 2);
-	private static final DateTimeFormatter	timeFormat	= DateTimeFormatter.ofPattern("HH:mm:ss:SSS");
-	private static final Writer				writer		= new FileWriter(FileDescriptor.out);
-	private static final ConsoleLogger		CONSOLE		= new ConsoleLogger();
+	private static final StackWalker       sw         = StackWalker.getInstance(Set.of(Option.RETAIN_CLASS_REFERENCE), 2);
+	private static final DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss:SSS");
+	private static final Writer            writer     = new FileWriter(FileDescriptor.out);
+	private static final ConsoleLogger     CONSOLE    = new ConsoleLogger();
 
 	private static boolean logc = ConfigLoader.logConsole();
-	private static int lvl = ConfigLoader.getLevel().value;
-	
-	private static final int	ERROR	= Level.ERROR.value;
-	private static final int	WARN	= Level.WARN.value;
-	private static final int	INFO	= Level.INFO.value;
-	private static final int	DEBUG	= Level.DEBUG.value;
+	private static int     lvl  = ConfigLoader.getLevel().value;
 
-	private final int	level	= lvl;
-	protected boolean	status	= true;
+	private static final int ERROR = Level.ERROR.value;
+	private static final int WARN  = Level.WARN.value;
+	private static final int INFO  = Level.INFO.value;
+	private static final int DEBUG = Level.DEBUG.value;
+
+	private final int level  = lvl;
+	protected boolean status = true;
 
 	protected abstract void write(String s);
 
@@ -52,100 +53,124 @@ public abstract class Logger implements AutoCloseable {
 		}
 	}
 
-	public final void error(final String s) {
-		if(status) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
-	}
-	
-	public final void error(final String t, final String v) {
-		if(status) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
-	}
-	
-	public final void error(final Object o) {
-		if(status) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
-	}
-	
-	public final void error(final Exception e) {
-		if(status) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
-	}
-	
-	
-	
-	
-	
-	public final void warn(final String s) {
-		if(status && level > ERROR) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
-	}
-	
-	public final void warn(final String t, final String v) {
-		if(status && level > ERROR) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
-	}
-	
-	public final void warn(final Object o) {
-		if(status && level > ERROR) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
-	}
-	
-	public final void warn(final Exception e) {
-		if(status && level > ERROR) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
-	}
-	
-	
-	
-	
-	public final void info(final String s) {
-		if(status && level > WARN) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
-	}
-	
-	public final void info(final String t, final String v) {
-		if(status && level > WARN) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
-	}
-	
-	public final void info(final Object o) {
-		if(status && level > WARN) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
-	}
-	
-	public final void info(final Exception e) {
-		if(status && level > WARN) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
-	}
-	
-	
-	
-	
-	public final void debug(final String s) {
-		if(status && level > INFO) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
-	}
-	
-	public final void debug(final String t, final String v) {
-		if(status && level > INFO) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
-	}
-	
-	public final void debug(final Object o) {
-		if(status && level > INFO) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
-	}
-	
-	public final void debug(final Exception e) {
-		if(status && level > INFO) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
-	}
-	
-	
-	
-	public final void traceback(final String s) {
+	public final void errorback(final String s) {
 		write(this, sw.walk(f -> f.skip(2).findFirst().get()), s);
 	}
-	
+
+	public final void error(final Supplier<Object> s) {
+		write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+	}
+
+	public final void error(final String s) {
+		if (status) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+	}
+
+	public final void error(final String t, final String v) {
+		if (status) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
+	}
+
+	public final void error(final Object o) {
+		if (status) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
+	}
+
+	public final void error(final Exception e) {
+		if (status) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
+	}
+
+	public final void warnback(final String s) {
+		if (status && level > ERROR) write(this, sw.walk(f -> f.skip(2).findFirst().get()), s);
+	}
+
+	public final void warn(final Supplier<Object> s) {
+		if (status && level > ERROR) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+	}
+
+	public final void warn(final String s) {
+		if (status && level > ERROR) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+	}
+
+	public final void warn(final String t, final String v) {
+		if (status && level > ERROR) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
+	}
+
+	public final void warn(final Object o) {
+		if (status && level > ERROR) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
+	}
+
+	public final void warn(final Exception e) {
+		if (status && level > ERROR) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
+	}
+
+	public final void infoback(final String s) {
+		if (status && level > WARN) write(this, sw.walk(f -> f.skip(2).findFirst().get()), s);
+	}
+
+	public final void info(final Supplier<Object> s) {
+		if (status && level > WARN) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+	}
+
+	public final void info(final String s) {
+		if (status && level > WARN) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+	}
+
+	public final void info(final String t, final String v) {
+		if (status && level > WARN) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
+	}
+
+	public final void info(final Object o) {
+		if (status && level > WARN) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
+	}
+
+	public final void info(final Exception e) {
+		if (status && level > WARN) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
+	}
+
+	public final void debugback(final String s) {
+		if (status && level > INFO) write(this, sw.walk(f -> f.skip(2).findFirst().get()), s);
+	}
+
+	public final void debug(final Supplier<Object> s) {
+		if (status && level > INFO) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+	}
+
+	public final void debug(final String s) {
+		if (status && level > INFO) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+	}
+
+	public final void debug(final String t, final String v) {
+		if (status && level > INFO) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
+	}
+
+	public final void debug(final Object o) {
+		if (status && level > INFO) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
+	}
+
+	public final void debug(final Exception e) {
+		if (status && level > INFO) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
+	}
+
+	public final void traceback(final String s) {
+		if (status && level > DEBUG) write(this, sw.walk(f -> f.skip(2).findFirst().get()), s);
+	}
+
+	public final void trace(final Supplier<Object> s) {
+		if (status && level > DEBUG) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+	}
+
 	public final void trace(final String s) {
-		if(status && level > DEBUG) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
+		if (status && level > DEBUG) write(this, sw.walk(f -> f.skip(1).findFirst().get()), s);
 	}
-	
+
 	public final void trace(final String t, final String v) {
-		if(status && level > DEBUG) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
+		if (status && level > DEBUG) write(this, sw.walk(f -> f.skip(1).findFirst().get()), t, v);
 	}
-	
+
 	public final void trace(final Object o) {
-		if(status && level > DEBUG) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
+		if (status && level > DEBUG) write(this, sw.walk(f -> f.skip(1).findFirst().get()), String.valueOf(o));
 	}
-	
+
 	public final void trace(final Exception e) {
-		if(status && level > DEBUG) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
+		if (status && level > DEBUG) write(this, sw.walk(f -> f.skip(1).findFirst().get()), stackTraceToString(e));
 	}
 
 	private static final void write(final Logger logger, final StackFrame frame, final String s) {
@@ -155,12 +180,25 @@ public abstract class Logger implements AutoCloseable {
 			sb.append(frame.getMethodName()).append("(").append(frame.getLineNumber()).append(") ").append(s).append("\r\n");
 			String ssb = sb.toString();
 			logger.write(ssb);
-			if(logc) writeConsole(ssb);
+			if (logc) writeConsole(ssb);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	private void write(Logger logger, StackFrame frame, Supplier<Object> s) {
+		try {
+			final StringBuilder sb = new StringBuilder(50);
+			sb.append("[").append(LocalDateTime.now().format(timeFormat)).append("] ").append(frame.getDeclaringClass().getSimpleName()).append(".");
+			sb.append(frame.getMethodName()).append("(").append(frame.getLineNumber()).append(") ").append(s.get().toString()).append("\r\n");
+			String ssb = sb.toString();
+			logger.write(ssb);
+			if (logc) writeConsole(ssb);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 
 	private static final void write(final Logger logger, final StackFrame frame, final String t, final String v) {
 		try {
@@ -169,17 +207,15 @@ public abstract class Logger implements AutoCloseable {
 			sb.append(frame.getMethodName()).append("(").append(frame.getLineNumber()).append(") ").append(t).append(" : ").append(v).append("\r\n");
 			String ssb = sb.toString();
 			logger.write(ssb);
-			if(logc) writeConsole(ssb);
+			if (logc) writeConsole(ssb);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	
 	public final void activate(boolean status) {
 		this.status = status;
 	}
-
 
 	public void close() throws Exception {}
 
@@ -194,28 +230,28 @@ public abstract class Logger implements AutoCloseable {
 	public static final Logger getConsole() {
 		return CONSOLE;
 	}
-	
+
 	private static final String stackTraceToString(Exception e) {
 		try {
 			StringWriter sw = new StringWriter();
-			PrintWriter pw = new PrintWriter(sw);
+			PrintWriter  pw = new PrintWriter(sw);
 			e.printStackTrace(pw);
 			return sw.toString();
 		} catch (Exception ex) {}
 		return "";
 	}
-	
-	public static final void scheduleDateChange(ScheduledExecutorService schedular) {
+
+	public static final void scheduleDateChange(final ScheduledExecutorService schedular) {
 		try {
 			schedular.scheduleAtFixedRate((ConsoleLogger) Logger.getConsole(), getEndOfDay(), 24 * 60 * 60, TimeUnit.SECONDS);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static final long getEndOfDay() {
-		Date date = new Date();
-		Calendar cal = Calendar.getInstance();
+		Date     date = new Date();
+		Calendar cal  = Calendar.getInstance();
 		cal.setTime(date);
 		cal.set(Calendar.HOUR_OF_DAY, cal.getMaximum(Calendar.HOUR_OF_DAY));
 		cal.set(Calendar.MINUTE, cal.getMaximum(Calendar.MINUTE));
