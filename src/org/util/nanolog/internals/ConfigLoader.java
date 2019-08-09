8bf8d02d927838e7217573ffd836b1225d6057fb
krishna.telgave <krishna.telgave@SIL_DEV076.saas.SILASPDC.com>
2019-08-10 01:02:24 +0530

    configuration will be loaded from ini/logging.properties.

--- src/org/util/nanolog/internals/ConfigLoader.java
+++ src/org/util/nanolog/internals/ConfigLoader.java
@@ -1,6 +1,7 @@
 package org.util.nanolog.internals;
 
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileReader;
 import java.util.Properties;
 
 import org.util.nanolog.Level;
@@ -10,16 +11,24 @@ public final class ConfigLoader {
 	private static final Properties properties = new Properties();
 
 	static {
-		try (InputStream in = ConfigLoader.class.getResourceAsStream("/nanolog.properties")) {
-			if (in == null) {
+		try {
+
+			final File file = new File("ini/logging.properties");
+			if(file.exists()) {
+				try(FileReader in = new FileReader(file)) {
+					properties.load(in);
+					if(!properties.containsKey("LEVEL")) properties.put("LEVEL", "TRACE");
+					if(!properties.containsKey("LOG_CONSOLE")) properties.put("LOG_CONSOLE", "Y");
+					System.out.println("Properties : " + properties);
+				} catch (Exception e) {e.printStackTrace();}
+			}
+			else {
 				System.err.println("Loading default logging properties\r\n" + "LEVEL=TRACE\r\n" + "LOG_CONSOLE=Y");
 				properties.put("LEVEL", "TRACE");
 				properties.put("LOG_CONSOLE", "Y");
-			} else properties.load(in);
-			System.out.println("Properties : " + properties);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+			}
+		
+		} catch (Exception e) {e.printStackTrace();}
 	}
 
 	public static final Level getLevel() {
@@ -35,5 +44,5 @@ public final class ConfigLoader {
 		if (properties.getProperty("LOG_CONSOLE") == null) return true;
 		return "Y".equalsIgnoreCase(properties.getProperty("LOG_CONSOLE"));
 	}
-
+	
 }
